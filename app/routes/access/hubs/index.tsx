7f58a25d93d6f4362e530e3894f517d76fb212c8
Michael Wu <mw10013@gmail.com>
2022-05-16 14:46:28 -0400

    style

--- app/routes/access/hubs/index.tsx
+++ app/routes/access/hubs/index.tsx
@@ -3,21 +3,8 @@ import { Link, useLoaderData } from "@remix-run/react";
 import { User } from "@prisma/client";
 import { prisma } from "~/db.server";
 import { requireUserIdForRole } from "~/session.server";
-import {
-  Header,
-  Main,
-  Table,
-  Td,
-  TdLink,
-  TdProminent,
-  Th,
-  ThSr,
-} from "~/components/lib";
-import {
-  CalendarIcon,
-  LocationMarkerIcon,
-  UsersIcon,
-} from "@heroicons/react/solid";
+import { Header } from "~/components/lib";
+import { LocationMarkerIcon } from "@heroicons/react/solid";
 
 type LoaderData = {
   accessHubs: Awaited<ReturnType<typeof getLoaderData>>;
@@ -73,116 +60,6 @@ export default function RouteComponent() {
           ))}
         </ul>
       </main>
-
-      <main className="mt-8 space-y-6">
-        <Table
-          headers={
-            <>
-              <Th>Name</Th>
-              <Th>Id</Th>
-              <Th>Description</Th>
-              <ThSr>View</ThSr>
-            </>
-          }
-        >
-          {accessHubs.map((i) => (
-            <tr key={i.id}>
-              <TdProminent>{i.name}</TdProminent>
-              <Td>{i.id}</Td>
-              <Td>{i.description}</Td>
-              <TdLink to={`${i.id}`}>View</TdLink>
-            </tr>
-          ))}
-        </Table>
-        <Example />
-      </main>
     </>
-  );
-}
-
-const positions = [
-  {
-    id: 1,
-    title: "Back End Developer",
-    type: "Full-time",
-    location: "Remote",
-    department: "Engineering",
-    closeDate: "2020-01-07",
-    closeDateFull: "January 7, 2020",
-  },
-  {
-    id: 2,
-    title: "Front End Developer",
-    type: "Full-time",
-    location: "Remote",
-    department: "Engineering",
-    closeDate: "2020-01-07",
-    closeDateFull: "January 7, 2020",
-  },
-  {
-    id: 3,
-    title: "User Interface Designer",
-    type: "Full-time",
-    location: "Remote",
-    department: "Design",
-    closeDate: "2020-01-14",
-    closeDateFull: "January 14, 2020",
-  },
-];
-
-function Example() {
-  return (
-    <div className="overflow-hidden bg-white shadow sm:rounded-md">
-      <ul className="divide-y divide-gray-200">
-        {positions.map((position) => (
-          <li key={position.id}>
-            <a href="." className="block hover:bg-gray-50">
-              <div className="px-4 py-4 sm:px-6">
-                <div className="flex items-center justify-between">
-                  <p className="truncate text-sm font-medium text-indigo-600">
-                    {position.title}
-                  </p>
-                  <div className="ml-2 flex flex-shrink-0">
-                    <p className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                      {position.type}
-                    </p>
-                  </div>
-                </div>
-                <div className="mt-2 sm:flex sm:justify-between">
-                  <div className="sm:flex">
-                    <p className="flex items-center text-sm text-gray-500">
-                      <UsersIcon
-                        className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                        aria-hidden="true"
-                      />
-                      {position.department}
-                    </p>
-                    <p className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
-                      <LocationMarkerIcon
-                        className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                        aria-hidden="true"
-                      />
-                      {position.location}
-                    </p>
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                    <CalendarIcon
-                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <p>
-                      Closing on{" "}
-                      <time dateTime={position.closeDate}>
-                        {position.closeDateFull}
-                      </time>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
   );
 }
