d0b73120ea7d7102b643fbf8491b862a100878c4
Michael Wu <mw10013@gmail.com>
2022-05-16 14:45:09 -0400

    style

--- app/routes/access/hubs/index.tsx
+++ app/routes/access/hubs/index.tsx
@@ -1,5 +1,5 @@
 import { json, LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { User } from "@prisma/client";
 import { prisma } from "~/db.server";
 import { requireUserIdForRole } from "~/session.server";
@@ -13,6 +13,11 @@ import {
   Th,
   ThSr,
 } from "~/components/lib";
+import {
+  CalendarIcon,
+  LocationMarkerIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
 
 type LoaderData = {
   accessHubs: Awaited<ReturnType<typeof getLoaderData>>;
@@ -35,10 +40,41 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function RouteComponent() {
   const { accessHubs } = useLoaderData<LoaderData>();
+
+  // With right-justified second column
+  // https://tailwindui.com/components/application-ui/lists/stacked-lists
   return (
     <>
       <Header title="Hubs" />
-      <Main>
+      <main>
+        <ul className="divide-y divide-gray-200">
+          {accessHubs.map((i) => (
+            <li key={i.id}>
+              <Link to={`${i.id}`} className="block hover:bg-gray-50">
+                <div className="rounded-lg px-4 py-4 ring-1 ring-gray-300 sm:px-6">
+                  <div className="flex items-center justify-between">
+                    <p className="truncate text-sm font-medium text-indigo-600">
+                      {i.name}
+                    </p>
+                    <p className="rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                      Active
+                    </p>
+                  </div>
+                  <p className="mt-2 flex items-center text-sm text-gray-500">
+                    <LocationMarkerIcon
+                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    {i.description}
+                  </p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+
+      <main className="mt-8 space-y-6">
         <Table
           headers={
             <>
@@ -58,7 +94,95 @@ export default function RouteComponent() {
             </tr>
           ))}
         </Table>
-      </Main>
+        <Example />
+      </main>
     </>
+  );
+}
+
+const positions = [
+  {
+    id: 1,
+    title: "Back End Developer",
+    type: "Full-time",
+    location: "Remote",
+    department: "Engineering",
+    closeDate: "2020-01-07",
+    closeDateFull: "January 7, 2020",
+  },
+  {
+    id: 2,
+    title: "Front End Developer",
+    type: "Full-time",
+    location: "Remote",
+    department: "Engineering",
+    closeDate: "2020-01-07",
+    closeDateFull: "January 7, 2020",
+  },
+  {
+    id: 3,
+    title: "User Interface Designer",
+    type: "Full-time",
+    location: "Remote",
+    department: "Design",
+    closeDate: "2020-01-14",
+    closeDateFull: "January 14, 2020",
+  },
+];
+
+function Example() {
+  return (
+    <div className="overflow-hidden bg-white shadow sm:rounded-md">
+      <ul className="divide-y divide-gray-200">
+        {positions.map((position) => (
+          <li key={position.id}>
+            <a href="." className="block hover:bg-gray-50">
+              <div className="px-4 py-4 sm:px-6">
+                <div className="flex items-center justify-between">
+                  <p className="truncate text-sm font-medium text-indigo-600">
+                    {position.title}
+                  </p>
+                  <div className="ml-2 flex flex-shrink-0">
+                    <p className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                      {position.type}
+                    </p>
+                  </div>
+                </div>
+                <div className="mt-2 sm:flex sm:justify-between">
+                  <div className="sm:flex">
+                    <p className="flex items-center text-sm text-gray-500">
+                      <UsersIcon
+                        className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      {position.department}
+                    </p>
+                    <p className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
+                      <LocationMarkerIcon
+                        className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      {position.location}
+                    </p>
+                  </div>
+                  <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                    <CalendarIcon
+                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <p>
+                      Closing on{" "}
+                      <time dateTime={position.closeDate}>
+                        {position.closeDateFull}
+                      </time>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
