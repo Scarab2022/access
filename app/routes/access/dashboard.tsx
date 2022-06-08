cdc30e87b20be2fc1c63298721dc4b334da23555
Michael Wu <mw10013@gmail.com>
2022-06-08 13:58:39 -0400

    dashboard: hub card

--- app/routes/access/dashboard.tsx
+++ app/routes/access/dashboard.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData, useFetcher, useLocation } from "@remix-run/react";
+import { useLoaderData, useFetcher, useLocation, Link } from "@remix-run/react";
 import type { AccessHub, User } from "@prisma/client";
 import { prisma } from "~/db.server";
 import { requireUserIdForRole } from "~/session.server";
 import { Switch } from "~/components/switch";
 import { PageHeader } from "~/components/page-header";
 import { Table } from "~/components/table";
+import { Badge } from "~/components/badge";
 
 export const handle = {
   breadcrumb: "Dashboard",
@@ -70,8 +71,8 @@ async function getLoaderData({ userId }: { userId: User["id"] }) {
     {}
   );
 
-  const accessHubsWithStats = accessHubs.map((v) => {
-    const accessPointsWithStats = v.accessPoints.map((p) => {
+  const accessHubsWithStats = accessHubs.map((h) => {
+    const accessPointsWithStats = h.accessPoints.map((p) => {
       return {
         ...p,
         _count: {
@@ -81,8 +82,9 @@ async function getLoaderData({ userId }: { userId: User["id"] }) {
       };
     });
     return {
-      ...v,
+      ...h,
       accessPoints: accessPointsWithStats,
+      connectionStatus: connectionStatus(h.heartbeatAt),
       _count: accessPointsWithStats.reduce(
         (acc, v) => {
           return {
@@ -95,7 +97,7 @@ async function getLoaderData({ userId }: { userId: User["id"] }) {
     };
   });
 
-  return { accessHubs, accessHubsWithStats, groupBy, pointsStats };
+  return { accessHubsWithStats };
 }
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -104,22 +106,28 @@ export const loader: LoaderFunction = async ({ request }) => {
   return json<LoaderData>(data);
 };
 
+const connectionStatusColors = {
+  Dying: "yellow",
+  Live: "green",
+  Dead: "red",
+} as const;
+
 function connectionStatus(heartbeatAt: AccessHub["heartbeatAt"]) {
+  // as const so infer is string literal not string
   if (heartbeatAt) {
     const deltaMs = Date.now() - new Date(heartbeatAt).getTime();
     if (deltaMs < 5 * 1000) {
-      return "Live";
+      return "Live" as const;
     }
     if (deltaMs < 10 * 1000) {
-      return "Dying";
+      return "Dying" as const;
     }
   }
-  return "Dead";
+  return "Dead" as const;
 }
 
 export default function RouteComponent() {
-  const { accessHubsWithStats, accessHubs, groupBy, pointsStats } =
-    useLoaderData<LoaderData>();
+  const { accessHubsWithStats } = useLoaderData<LoaderData>();
   const poll = useFetcher<LoaderData>();
   const [isPolling, setIsPolling] = React.useState(false);
   const location = useLocation();
@@ -142,32 +150,67 @@ export default function RouteComponent() {
           </Switch.Group>
         }
       />
-      <main>
+      <main className="space-y-8">
+        {(poll.data?.accessHubsWithStats ?? accessHubsWithStats).map((h) => {
+          // Simple cards: https://tailwindui.com/components/application-ui/lists/grid-lists
+          return (
+            <div
+              key={h.id}
+              className="grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3"
+            >
+              <div className="col-span-1 flex rounded-md shadow-sm">
+                {/* <div className="col-span-1 flex rounded-md border border-gray-300"> */}
+                <div className="flex w-16 flex-shrink-0 items-center justify-center rounded-l-md bg-pink-600 text-sm font-medium text-white">
+                  HUB
+                </div>
+                <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-t border-r border-b border-gray-200 bg-white">
+                  <div className="flex-1 truncate px-4 py-2 text-sm">
+                    <Link
+                      to={`../hubs/${h.id}`}
+                      className="font-medium text-gray-900 hover:text-gray-600"
+                    >
+                      {h.name}
+                    </Link>
+                    <p className="text-gray-500">
+                      Last 24 hours {h._count.grant} grants and {h._count.deny}{" "}
+                      denies.
+                    </p>
+                  </div>
+                  <div className="flex-shrink-0  pr-2">
+                    <Badge color={connectionStatusColors[h.connectionStatus]}>{h.connectionStatus}</Badge>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
         <pre>{JSON.stringify(accessHubsWithStats, null, 2)}</pre>
-        <pre>{JSON.stringify(pointsStats, null, 2)}</pre>
-        {/* <pre>{JSON.stringify(groupBy, null, 2)}</pre> */}
-        <pre>{JSON.stringify(accessHubs, null, 2)}</pre>
-        {/* <Table
-          headers={
-            <>
-              <Table.Th>Hub</Table.Th>
-              <Table.Th>Name</Table.Th>
-              <Table.Th>Connection</Table.Th>
-              <Table.Th sr>View</Table.Th>
-            </>
-          }
-        >
-          {(poll.data?.accessPoints ?? accessPoints).map((i) => (
-            <tr key={i.id}>
-              <Table.Td>{i.accessHub.name}</Table.Td>
-              <Table.Td prominent>{i.name}</Table.Td>
-              <Table.Td>{connectionStatus(i.accessHub.heartbeatAt)}</Table.Td>
-              <Table.TdLink to={`../hubs/${i.accessHubId}/points/${i.id}`}>
-                View
-              </Table.TdLink>
-            </tr>
-          ))}
-        </Table> */}
+        {(poll.data?.accessHubsWithStats ?? accessHubsWithStats).map((h) => {
+          return (
+            <Table
+              key={h.id}
+              headers={
+                <>
+                  <Table.Th>Hub</Table.Th>
+                  <Table.Th>Name</Table.Th>
+                  <Table.Th>Connection</Table.Th>
+                  <Table.Th sr>View</Table.Th>
+                </>
+              }
+            >
+              {h.accessPoints.map((i) => (
+                <tr key={i.id}>
+                  <Table.Td>{h.name}</Table.Td>
+                  <Table.Td prominent>{i.name}</Table.Td>
+                  <Table.Td>{connectionStatus(h.heartbeatAt)}</Table.Td>
+                  <Table.TdLink to={`../hubs/${h.id}/points/${i.id}`}>
+                    View
+                  </Table.TdLink>
+                </tr>
+              ))}
+            </Table>
+          );
+        })}
       </main>
     </>
   );
