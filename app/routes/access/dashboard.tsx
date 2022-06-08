044a0ed171e94e7bb57f65bfd283c1a8d9c33890
Michael Wu <mw10013@gmail.com>
2022-06-08 17:14:55 -0400

    dashboard: point

--- app/routes/access/dashboard.tsx
+++ app/routes/access/dashboard.tsx
@@ -7,7 +7,6 @@ import { prisma } from "~/db.server";
 import { requireUserIdForRole } from "~/session.server";
 import { Switch } from "~/components/switch";
 import { PageHeader } from "~/components/page-header";
-import { Table } from "~/components/table";
 import { Badge } from "~/components/badge";
 
 export const handle = {
@@ -24,6 +23,7 @@ async function getLoaderData({ userId }: { userId: User["id"] }) {
     orderBy: [{ name: "asc" }],
     include: {
       accessPoints: {
+        orderBy: [{ position: "asc" }],
         include: {
           _count: {
             select: { accessUsers: true },
@@ -150,7 +150,7 @@ export default function RouteComponent() {
           </Switch.Group>
         }
       />
-      <main className="space-y-8">
+      <main className=" space-y-8">
         {(poll.data?.accessHubsWithStats ?? accessHubsWithStats).map((h) => {
           // Simple cards: https://tailwindui.com/components/application-ui/lists/grid-lists
           return (
@@ -177,40 +177,47 @@ export default function RouteComponent() {
                     </p>
                   </div>
                   <div className="flex-shrink-0  pr-2">
-                    <Badge color={connectionStatusColors[h.connectionStatus]}>{h.connectionStatus}</Badge>
+                    <Badge color={connectionStatusColors[h.connectionStatus]}>
+                      {h.connectionStatus}
+                    </Badge>
                   </div>
                 </div>
               </div>
+              {h.accessPoints.map((p) => {
+                return (
+                  <div
+                    key={p.id}
+                    className="col-span-1 flex rounded-md shadow-sm"
+                  >
+                    <div className="flex w-16 flex-shrink-0 items-center justify-center rounded-l-md bg-purple-600 text-sm font-medium text-white">
+                      {p.position}
+                    </div>
+                    <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-t border-r border-b border-gray-200 bg-white">
+                      <div className="flex-1 truncate px-4 py-2 text-sm">
+                        <Link
+                          to={`../hubs/${h.id}/points/${p.id}`}
+                          className="font-medium text-gray-900 hover:text-gray-600"
+                        >
+                          {p.name}
+                        </Link>
+                        <p className="text-gray-500">
+                          Last 24 hours {p._count.grant} grants and{" "}
+                          {p._count.deny} denies.
+                        </p>
+                      </div>
+                      <div className="flex-shrink-0  pr-2">
+                        <p className="text-gray-500">
+                          {p._count.accessUsers} users
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           );
         })}
-        <pre>{JSON.stringify(accessHubsWithStats, null, 2)}</pre>
-        {(poll.data?.accessHubsWithStats ?? accessHubsWithStats).map((h) => {
-          return (
-            <Table
-              key={h.id}
-              headers={
-                <>
-                  <Table.Th>Hub</Table.Th>
-                  <Table.Th>Name</Table.Th>
-                  <Table.Th>Connection</Table.Th>
-                  <Table.Th sr>View</Table.Th>
-                </>
-              }
-            >
-              {h.accessPoints.map((i) => (
-                <tr key={i.id}>
-                  <Table.Td>{h.name}</Table.Td>
-                  <Table.Td prominent>{i.name}</Table.Td>
-                  <Table.Td>{connectionStatus(h.heartbeatAt)}</Table.Td>
-                  <Table.TdLink to={`../hubs/${h.id}/points/${i.id}`}>
-                    View
-                  </Table.TdLink>
-                </tr>
-              ))}
-            </Table>
-          );
-        })}
+        {/* <pre>{JSON.stringify(accessHubsWithStats, null, 2)}</pre> */}
       </main>
     </>
   );
