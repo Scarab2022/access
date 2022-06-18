c1dda208df56291ba49b88bb555ec4afaed25a98
Michael Wu <mw10013@gmail.com>
2022-06-18 12:40:53 -0400

    login: zod

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -4,12 +4,19 @@ import type {
   MetaFunction,
 } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
+import {
+  Form as RemixForm,
+  Link,
+  useActionData,
+  useSearchParams,
+} from "@remix-run/react";
 import * as React from "react";
+import { z, ZodError } from "zod";
 
 import { createUserSession, getUserId } from "~/session.server";
 import { verifyLogin } from "~/models/user.server";
 import { validateEmail } from "~/utils";
+import { Form } from "~/components/form";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await getUserId(request);
@@ -17,46 +24,79 @@ export const loader: LoaderFunction = async ({ request }) => {
   return json({});
 };
 
-interface ActionData {
-  errors?: {
-    email?: string;
-    password?: string;
-  };
-}
+const FieldValues = z
+  .object({
+    email: z
+      .string()
+      .min(1)
+      .max(50)
+      .email()
+      .transform((v) => v.toLowerCase()),
+    password: z.string().min(8).max(50),
+    redirectTo: z.string(),
+    remember: z.string().transform((v) => v === "on"),
+  })
+  .strict();
+
+// interface ActionData {
+//   errors?: {
+//     email?: string;
+//     password?: string;
+//   };
+// }
+
+type ActionData = {
+  formErrors?: ZodError["formErrors"];
+  // fieldValues?: any;
+};
 
 export const action: ActionFunction = async ({ request }) => {
-  const formData = await request.formData();
-  const email = formData.get("email");
-  const password = formData.get("password");
-  const redirectTo = formData.get("redirectTo");
-  const remember = formData.get("remember");
+  // const formData = await request.formData();
+  // const email = formData.get("email");
+  // const password = formData.get("password");
+  // const redirectTo = formData.get("redirectTo");
+  // const remember = formData.get("remember");
 
-  if (!validateEmail(email)) {
+  // if (!validateEmail(email)) {
+  //   return json<ActionData>(
+  //     { errors: { email: "Email is invalid" } },
+  //     { status: 400 }
+  //   );
+  // }
+
+  // if (typeof password !== "string") {
+  //   return json<ActionData>(
+  //     { errors: { password: "Password is required" } },
+  //     { status: 400 }
+  //   );
+  // }
+
+  // if (password.length < 8) {
+  //   return json<ActionData>(
+  //     { errors: { password: "Password is too short" } },
+  //     { status: 400 }
+  //   );
+  // }
+
+  // WARNING: Object.fromEntries(formData): if formData.entries() has 2 entries with the same key, only 1 is taken.
+  const fieldValues = Object.fromEntries(await request.formData());
+  const parseResult = FieldValues.safeParse(fieldValues);
+  if (!parseResult.success) {
     return json<ActionData>(
-      { errors: { email: "Email is invalid" } },
+      {
+        formErrors: parseResult.error.formErrors,
+        // fieldValues,
+      },
       { status: 400 }
     );
   }
-
-  if (typeof password !== "string") {
-    return json<ActionData>(
-      { errors: { password: "Password is required" } },
-      { status: 400 }
-    );
-  }
-
-  if (password.length < 8) {
-    return json<ActionData>(
-      { errors: { password: "Password is too short" } },
-      { status: 400 }
-    );
-  }
-
+  const { email, password, redirectTo, remember } = parseResult.data;
   const user = await verifyLogin(email, password);
 
   if (!user) {
     return json<ActionData>(
-      { errors: { email: "Invalid email or password" } },
+      // { errors: { email: "Invalid email or password" } },
+      { formErrors: ["Invalid email or password"] },
       { status: 400 }
     );
   }
@@ -64,13 +104,19 @@ export const action: ActionFunction = async ({ request }) => {
   return createUserSession({
     request,
     userId: user.id,
-    remember: remember === "on" ? true : false,
+    // remember: remember === "on" ? true : false,
+    remember,
     redirectTo:
-      typeof redirectTo === "string" && redirectTo !== ""
+      redirectTo !== ""
         ? redirectTo
         : user.role === "admin"
         ? "/admin/dashboard"
         : "/access/dashboard",
+    // typeof redirectTo === "string" && redirectTo !== ""
+    //   ? redirectTo
+    //   : user.role === "admin"
+    //   ? "/admin/dashboard"
+    //   : "/access/dashboard",
   });
 };
 
@@ -84,14 +130,14 @@ export default function LoginPage() {
   const [searchParams] = useSearchParams();
   // const redirectTo = searchParams.get("redirectTo") || "/access/dashboard";
   const redirectTo = searchParams.get("redirectTo") || "";
-  const actionData = useActionData() as ActionData;
+  const actionData = useActionData<ActionData>();
   const emailRef = React.useRef<HTMLInputElement>(null);
   const passwordRef = React.useRef<HTMLInputElement>(null);
 
   React.useEffect(() => {
-    if (actionData?.errors?.email) {
+    if (actionData?.formErrors?.fieldErrors?.email) {
       emailRef.current?.focus();
-    } else if (actionData?.errors?.password) {
+    } else if (actionData?.formErrors?.fieldErrors?.password) {
       passwordRef.current?.focus();
     }
   }, [actionData]);
@@ -120,9 +166,34 @@ export default function LoginPage() {
         </p>
       </div>
 
+      {/* <Form method="post" noValidate replace>
+        <Form.Content>
+        <Form.Field
+              id="email"
+              label="Email"
+              errors={actionData?.errors?.email ? [actionData.errors.email] : undefined}
+            >
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                autoFocus={true}
+                autoComplete="email"
+                defaultValue={
+                  actionData?.fieldValues
+                    ? actionData.fieldValues.name
+                    : accessHub.name
+                }
+              />
+            </Form.Field>
+        </Form.Content>
+      </Form> */}
+
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="py-8 px-4 sm:px-10">
-          <Form method="post" className="space-y-6" noValidate>
+          {/* <RemixForm method="post" className="space-y-6" noValidate> */}
+          <RemixForm method="post" className="space-y-6" noValidate>
             <div>
               <label
                 htmlFor="email"
@@ -139,13 +210,17 @@ export default function LoginPage() {
                   name="email"
                   type="email"
                   autoComplete="email"
-                  aria-invalid={actionData?.errors?.email ? true : undefined}
+                  aria-invalid={
+                    actionData?.formErrors?.fieldErrors?.email
+                      ? true
+                      : undefined
+                  }
                   aria-describedby="email-error"
                   className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 />
-                {actionData?.errors?.email && (
+                {actionData?.formErrors?.fieldErrors?.email && (
                   <div className="pt-1 text-red-700" id="email-error">
-                    {actionData.errors.email}
+                    {actionData.formErrors.fieldErrors.email.join(". ")}
                   </div>
                 )}
               </div>
@@ -165,14 +240,18 @@ export default function LoginPage() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
-                  aria-invalid={actionData?.errors?.password ? true : undefined}
+                  aria-invalid={
+                    actionData?.formErrors?.fieldErrors?.password
+                      ? true
+                      : undefined
+                  }
                   aria-describedby="password-error"
                   required
                   className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 />
-                {actionData?.errors?.password && (
+                {actionData?.formErrors?.fieldErrors?.password && (
                   <div className="pt-1 text-red-700" id="password-error">
-                    {actionData.errors.password}
+                    {actionData.formErrors?.fieldErrors.password.join(". ")}
                   </div>
                 )}
               </div>
@@ -213,7 +292,7 @@ export default function LoginPage() {
                 Sign in
               </button>
             </div>
-          </Form>
+          </RemixForm>
         </div>
       </div>
     </div>
